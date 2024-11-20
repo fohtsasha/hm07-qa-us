b862dff8c228c37d106cb9dc7e6a7eab14bc64e5
Sasha Fokht <fohtsasha@gmail.com>
2024-11-20 20:43:00 +0100

    POST Test

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -2,19 +2,53 @@
 const config = require('../config');
 
 const requestBody = {
-    // put your body here
+   "deliveryTime": 9,
+    "productsCount": 10,
+    "productsWeight": 11
 }
-
-test('', async () => {
+test('Status should be 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await 
+		fetch(`${config.API_URL}/speedy/v1/calculate`,
+			{
 			method: 'POST',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
+		const actualStatus=response.status;
+		const data = await response.json();
+
+		expect(actualStatus).toBe(200);
 	} catch (error) {
 		console.error(error);
 	}
 });
+
+
+test('Speedy delivery should have all required fields ', async () => {
+    try {
+		const response = await fetch(`${config.API_URL}/speedy/v1/calculate`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		const data = await response.json();
+const expectedSpeedyDelivery = ["name", "clientDeliveryCost","toBeDeliveredTime","hostDeliveryCost","isItPossibleToDeliver"]
+
+const receivedFields = Object.keys(data);
+		expectedSpeedyDelivery.forEach(field => {
+			expect(receivedFields).toContain(field);
+
+		});
+
+} catch (error) {
+		console.error(error);
+	}
+});
+
+
+
