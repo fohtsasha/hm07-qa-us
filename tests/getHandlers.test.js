4808daa36531c044e4d8c63b6553ea6384b4bc90
Sasha Fokht <fohtsasha@gmail.com>
2024-11-20 18:48:26 +0100

    Get Tests complete

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,9 +1,28 @@
-// eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Get Status should be 200', async () => {
+	let actualStatus;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/couriers`);
+		actualStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualStatus).toBe(200);
+});
+
+test('Response body should have all types of deliveries', async () => {
+	let response;
+	try {
+		response = await fetch(`${config.API_URL}/api/v1/couriers`);
+		const data = await response.json();
+		const expectedDeliveryTypes = ["Order and Go", "Speedy", "Fast Delivery", "Food Service"];
+
+		expectedDeliveryTypes = data.map(d => d.name);
+		expectedDeliveryTypes.forEach(type => {
+			expect(receivedDeliveryTypes).toContain(type);
+
+		});
 	} catch (error) {
 		console.error(error);
 	}
