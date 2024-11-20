677442e14b2f20117b0b7e29b5ffffee7c19c6ac
Sasha Fokht <fohtsasha@gmail.com>
2024-11-20 21:40:32 +0100

    DELETE Test

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,11 +1,28 @@
-// eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Status should be 200 OK', async () => {
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+			method: 'DELETE',
+		});
+		const actualStatus=response.status;
+		const data = await response.json();
+
+		expect(actualStatus).toBe(200);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+
+
+test('Kit has been deleted', async () => {
     try {
 		const response = await fetch(`${config.API_URL}/your/endpoint`, {
 			method: 'DELETE',
 		});
+const data = await json();
+expect(data.ok).toBe(true);
 	} catch (error) {
 		console.error(error);
 	}
