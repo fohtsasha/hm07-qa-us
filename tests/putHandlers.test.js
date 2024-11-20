6aebb9613158b88b38f1aa0fa1ad4fa8e60683bf
Sasha Fokht <fohtsasha@gmail.com>
2024-11-20 21:33:22 +0100

    PUT Test

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,18 +1,40 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-const requestBody = {}
+const requestBody = {
+    "price": 175
+}
 
-test('', async () => {
+test('Status code should be 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/products/7`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
+		const actualStatus=response.status;
+		const data = await response.json();
+		expect (actualStatus).toBe(200);
 	} catch (error) {
+		console.error(error);
+	}
+});
+
+
+test('Price has been changed', async () => {
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/products/7`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		const data = await response.json();
+		expect(data.ok).toBe(true);
+	}  catch (error) {
 		console.error(error);
 	}
 });
