228f154842854d90b119c7ba572fef448683625e
fohtsasha <fohtsasha@gmail.com>
2024-11-20 12:01:15 +0100

    config.js URL added

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-91df9c25-e937-45e5-8a0f-5b0be0965a65.containerhub.tripleten-services.com/"
 }
