dcbcb05a7714a0cbe4e806a7171d5e0c0be35d65
Sasha Fokht <fohtsasha@gmail.com>
2024-11-21 13:08:43 +0100

    url update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-0538f31f-bb87-4c55-ae2e-ba1264832517.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-01f6c810-3fef-4ebb-bb64-e0a80733b773.containerhub.tripleten-services.com"
 }
